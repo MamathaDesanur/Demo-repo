8aa61a579c7a4884d9df1e8ad0efdb5d9efb2c4c
Mamatha Desanur <mamtha.d528@gmail.com>
2023-03-13 10:38:08 +1100

    second git commit

---
+++ src/main/java/gitmavenproject/GitDemomavenproject/App.java
@@ -0,0 +1,13 @@
+package gitmavenproject.GitDemomavenproject;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
